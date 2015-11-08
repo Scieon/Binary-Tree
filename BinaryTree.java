5f93c1d45b95f3cf92793d24bb938a129e769bb9
Scieon <anhkhoi.vunguyen@gmail.com>
2015-11-07 23:22:34 -0500

    Added clone method and position iterator

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,14 +1,25 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 
 public class BinaryTree<T> {
 
-	class Node<T> implements Position<T>,  Iterator<T> {
+	class Node<T> implements Position<T> {
 
 		private T data;
 		private Node<T> parentNode;
 		private Node<T> leftChild;
 		private Node<T> rightChild;
 
+		/**
+		 * Parameterized constructor.
+		 * @param data the specific parameter 
+		 * @param parentNode the direct ancestor of the current node.
+		 * @param leftChild the direct left descendant of the current node.
+		 * @param rightChild the direct right descendant of the current node.
+		 */
 		public Node(T data, Node<T> parentNode, Node<T> leftChild, Node<T> rightChild){
 			this.data = data; 
 			this.parentNode = parentNode;
@@ -16,90 +27,133 @@ public class BinaryTree<T> {
 			this.rightChild = rightChild;
 		}
 
+		/**
+		 * Assessor method for the parent node.
+		 * @return the parent node.
+		 */
 		public Node<T> getParent(){
 			return parentNode;
 		}
 
+		/**
+		 * Assessor method for the element held at the current node.
+		 * @return a generic data type.
+		 */
 		public T getElement(){
 			return data;
 		}
-
-		public Node<T> copyLeft() {
-
-			Node<T> left = null;
-			Node<T> right = null;
-			Node<T> parent = null;
-
-			if(this.leftChild != null){
-				left = this.leftChild.copyLeft();
-			}
-			if (this.rightChild != null) {
-				right = this.rightChild.copyLeft();
-			}
-
-			return new Node<T>(data,parent,left, right);
-		}
-
-		public Node<T> copyRight() {
-
-			Node<T> left = null;
-			Node<T> right = null;
-			Node<T> parent = null;
-
-			if(this.leftChild != null){
-				left = this.leftChild.copyRight();
-			}
-			if (this.rightChild != null) {
-				right = this.rightChild.copyRight();
-			}
-
-			return new Node<T>(data,parent,left, right);
-		}
-
 
 
 	}//End of inner class
 
 	private Node<T> root = null; //Initially there is no root for the tree.
-	private int size = 0;
+	private int size = 0; //Initially size of the tree is 0.
 
+	//Default constructor initializing the tree to null.
 	public BinaryTree(){}
 
+	public BinaryTree(BinaryTree<T> binaryTree) {
+		// TODO Auto-generated constructor stub
 
-	public void addRoot(T data){
-		root = new Node<T>(data,null,null,null);
-		size = 1;
+		root = clone(binaryTree.root);
+		size = binaryTree.size;
 	}
 
+	/**
+	 * Adds a root node to a tree.
+	 * @param data a generic data type that will be held in the newly created node.
+	 */
+	public void addRoot(T data){
+		root = new Node<T>(data,null,null,null);
+		size = 1; //Resets the size of the tree back to 1. 
+	}
+
+	/**
+	 * This method adds a left node to a given node.
+	 * @param data a generic data type that will be held in the newly created node.
+	 * @param p the node to add the left child to.
+	 */
 	public void addLeftChild(T data, Position<T> p){
 
 		Node<T> parentNode = (Node<T>)p;
 		Node<T> leftChild = new Node<T>(data,parentNode,null,null);
 		parentNode.leftChild = leftChild;
-		size++;
+		size++; //Adding a child increases the size of the tree by 1.
 	}
 
+	/**
+	 * This method adds a right node to a given node.
+	 * @param data a generic data type that will be held in the newly created node.
+	 * @param p the node to add the right child to.
+	 */
 	public void addRightChild(T data, Position<T> p){
 		Node<T> parentNode = (Node<T>)p;
 		Node<T> rightChild = new Node<T>(data,parentNode,null,null);
 		parentNode.rightChild = rightChild;
-		size++;
+		size++; //Adding a child increases the size of the tree by 1.
 	}
 
-
+	/**
+	 * @return root of the tree as a generic position to avoid privacy leak.
+	 */
 	public Position<T> root(){
 		return root;
 	}
 
-
+	/**
+	 * @param p 
+	 * @return
+	 */
 	public Position<T>parent(Position<T> p){
 		Node<T> node = (Node<T>)p;
 		return node.getParent();
 	}
 
-	public Iterable<Position<T>>children(Position <T> p){
+	public Iterable<Position<T>> children(Position <T> p){
 
-		return null;//
+		ArrayList<Position<T>> childList = new ArrayList<Position<T>>(2);
+		if(left(p) != null)
+			childList.add(left(p));
+
+		if(right(p)!=null)
+			childList.add(right(p));
+
+		return childList;
+
+	}
+
+	public void printInorder(){
+		printInorder(root);
+		System.out.println();
+	}
+	public void printInorder(Node<T> p){
+
+		if(p.leftChild != null)
+			printInorder(p.leftChild);
+		System.out.print(p.getElement() + " ");
+		if(p.rightChild != null)
+			printInorder(p.rightChild);
+	}
+
+
+	public Iterable<Position<T>> getPosIterator(){
+		ArrayList<Position<T>> posList = new ArrayList<Position<T>>();
+		positions(posList,root);
+		System.out.println(); //For spacing issues omit this line
+		return posList;
+
+	}
+	public void positions(ArrayList<Position<T>> arr,Node<T> p){
+		//ArrayList<Position<T>> posList = new ArrayList<Position<T>>();
+
+		if(p.leftChild != null)
+			positions(arr,p.leftChild);
+
+		arr.add(p);
+		System.out.print(p.getElement() + " "); //Just to see the actual elements, omit this line
+		if(p.rightChild != null)
+			positions(arr,p.rightChild);
+
 	}
 
 	public int numChildren(Position <T> p){
@@ -177,12 +231,26 @@ public class BinaryTree<T> {
 		return root == null;
 	}
 
-	public int height(){
-
-		return 0;
+	/**
+	 * Recursive method to find a height of the tree at a given position.
+	 * @param p
+	 * @return
+	 */
+	public int height( Position<T> p ){
+		int height = 0; //Initially height 
+		for(Position<T> c : children(p))
+			height= Math.max(height,1+height(c));
+		return height;
 	}
 
-	private Node<T> temp = root;
+	/**
+	 * @return the height of the tree.
+	 */
+	public int height(){	
+		return height(root);
+	}
+
+
 
 	public int numLeaf(Node<T> p){
 
@@ -193,20 +261,37 @@ public class BinaryTree<T> {
 		else
 			return numLeaf(p.leftChild) + numLeaf(p.rightChild);
 	}
-	
+
 	public int numLeaf(){
 		return numLeaf(root);
 	}
-	
-	
+
+
+
+	public Node<T> clone(Position<T>p){
+		Node<T> node = (Node<T>)p;
+
+		Node<T> left = (Node<T>)p;
+		Node<T> right = (Node<T>)p;
+
+		if(node.leftChild != null){
+			left = (Node<T>)clone(left(p));
+			node.leftChild = left;
+			left.parentNode = node;
+
+		}
+		if(node.rightChild != null){
+			right = (Node<T>)clone(right(p));
+			node.rightChild = right;
+			right.parentNode = node;
+		}
+
+		return node; //Return the node which is a clone of all other nodes except for the root
+
+	}
 	public BinaryTree<T> clone(){
 
-
-		BinaryTree<T> cloneTree = new BinaryTree();
-		cloneTree.addRoot(root.getElement());
-		cloneTree.root.leftChild = (root.copyLeft());
-		cloneTree.root.rightChild = (root.copyRight());
-		return cloneTree;
+		return new BinaryTree<T>(this);
 	}
 
 
